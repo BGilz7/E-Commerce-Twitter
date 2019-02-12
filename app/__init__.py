775957e4a1ef1000c9114d73bdd467e3e3e4c990
BGilz7 <B-Gil@msn.com>
2019-02-12 12:33:16 -0500

    posts paged templated out. To do: add backend to posts page

---
+++ app/__init__.py
@@ -0,0 +1,15 @@
+# import Flask class from flask module
+from flask import Flask
+from flask_bootstrap import Bootstrap
+
+
+
+# create instance of app variable
+app = Flask(__name__)
+
+# all other variable instances need to come after the app instance
+bootstrap = Bootstrap(app)
+
+
+# once app variable is creating import the routes to load home page
+from app import routes
