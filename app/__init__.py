7c67a02e5b5f724fe61546a340ce7d4289ca2d53
BGilz7 <B-Gil@msn.com>
2019-02-14 12:35:34 -0500

    Added models, TODO: update routes

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,8 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import Config
-
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
 # create instance of app variable
 app = Flask(__name__)
@@ -10,6 +11,11 @@ app = Flask(__name__)
 # all other variable instances need to come after the app instance
 bootstrap = Bootstrap(app)
 app.config.from_object(Config)
+
+# app variables for database usage
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
 
 # once app variable is creating import the routes to load home page
 from app import routes
