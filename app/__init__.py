be7ec7cb4554e41783559156cccb03cf7c1dfb43
BGilz7 <B-Gil@msn.com>
2019-02-15 10:17:00 -0500

    Login and Register routes rendering, TODO: Add user table to db

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_bootstrap import Bootstrap
 from config import Config
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 # create instance of app variable
 app = Flask(__name__)
@@ -15,6 +16,12 @@ app.config.from_object(Config)
 # app variables for database usage
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+# app variables for login
+# login = LoginManager(app)
+
+# when a page requires somebody to login, the application will instead route htme to the correct described below
+# login.login_view = 'login'
 
 
 # once app variable is creating import the routes to load home page
